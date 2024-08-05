85c6bdeab5ac0a1916b6012b03e4a23214903406
Yehor Dykhov <hyoustone@yahoo.com>
2024-08-05 21:42:31 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let ip = "127.0.0.1";
+    let port = "4221";
+
+    let listener = TcpListener::bind(format!("{}:{}", ip, port)).unwrap();
+
+    println!("Http server started on: {}:{}", ip, port);
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut tcp_stream) => {
                 println!("accepted new connection");
+                let response = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
+                tcp_stream.write_all(response).expect("send response 200");
             }
             Err(e) => {
                 println!("error: {}", e);
